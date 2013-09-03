922d505be7afeb67a03d963ec2abac51e022bf3c
Nick Small <nick@shopify.com>
2013-09-03 14:27:47 -0400

    Slow down async requiring to fix load order. Blech.

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,16 @@ $(function() {
       $('.try-it-live').html('Loading...');
 
       setTimeout(function(){
-        $.getScript('lib/dist/batman.jquery.js')
-        $.getScript('lib/extras/batman.rails.js')
+        // uses requre.js
+        // jklawl that would be so smrt
+        $.getScript('lib/dist/batman.jquery.js', function() {
+          $.getScript('lib/extras/batman.rails.js', function() {
+            $.getScript('js/try.js', function() {
+              $(".intro").addClass('expanded').css({height:920});
+            })
+          })
+        })
+
         $.getScript('lib/codemirror.js', function() {
           $.getScript('lib/modes/coffeescript.js')
           $.getScript('lib/modes/ruby.js')
@@ -17,10 +25,6 @@ $(function() {
 
         $('<link rel="stylesheet" href="css/codemirror.css" />').appendTo('head')
         $('<link rel="stylesheet" href="css/solarized.css" />').appendTo('head')
-
-        $.getScript('js/try.js', function() {
-          $(".intro").addClass('expanded').css({height:920});
-        });
       }, 0);
     }
   });
